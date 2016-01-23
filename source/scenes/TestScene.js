34da518fd3c7edcaee16bf00e9993c2c5cfd1806
Justin Dao <tjustindao@gmail.com>
2016-01-22 21:34:53 -0800

    Refactor player class:q

--- source/scenes/TestScene.js
+++ source/scenes/TestScene.js
@@ -1,12 +1,12 @@
 var pixi = require('pixi.js');
 var colors = require('../colors');
-var Player = require("../objects/Player");
+var objects = require("../objects");
 var keyboard = require("../keyboard");
 
 function TestScreen() {
   this.stage = new pixi.Container();
 
-  this.player = new Player(new pixi.Sprite.fromImage('/graphics/space_guy.png'));
+  this.player = new objects.Player(new pixi.Sprite.fromImage('/graphics/space_guy.png'));
 
   this.player.sprite.x = 300;
   this.player.sprite.y = 300;

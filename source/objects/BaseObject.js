e1b8ace053405a83330c0c7cf42f4cef82f7c974
Andy Barron <AndrewLBarron@gmail.com>
2016-02-01 23:02:56 -0800

    Change BaseObject to class

--- source/objects/BaseObject.js
+++ source/objects/BaseObject.js
@@ -1,17 +1,13 @@
 var EventEmitter = require('eventemitter3');
-var extend = require('../extend');
 var assets = require('../assets');
 
-function BaseObject() {
-  EventEmitter.call(this);
-}
-
-extend(EventEmitter, BaseObject, {
-  linkEventToSound: function linkEventToSound(event, sound) {
+module.exports = class BaseObject extends EventEmitter {
+  constructor() {
+    super()
+  }
+  linkEventToSound(event, sound) {
     this.on(event, function() {
       assets.playSound(sound);
     })
-  },
-});
-
-module.exports = BaseObject;
+  }
+}
